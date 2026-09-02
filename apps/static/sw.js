11bf4d19e545aba467a9423aa36db048171fe603
chiquilloalexis <arcm27250202@soy.sena.edu.co>
2026-09-02 13:45:13 -0500

    Implement service worker caching and fetch handling

--- apps/static/sw.js
+++ apps/static/sw.js
@@ -1,11 +1,59 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = "logidrink-v1";
+
+const ARCHIVOS_CACHE = [
+    "/",
+    "/static/manifest.json",
+    "/static/icons/icon-192.png",
+    "/static/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ARCHIVOS_CACHE);
+            })
+            .then(() => {
+                return self.skipWaiting();
+            })
+    );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys
+                    .filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        }).then(() => {
+            return self.clients.claim();
+        })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", (event) => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+
 });
